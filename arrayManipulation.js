f90fd7fc82fee761c810dec44db4026a2079c031
Kuuku Ampah <ampah.kuuku@gmail.com>
2024-05-17 23:10:51 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,6 +15,28 @@ function processArray(arr){
     return array;
 }
 
-const num = [5,6,7,8,9];
+const num = [5,6,7,8,9,12];
 const array = processArray(num);
 console.log(array);
+
+function formatArrayStrings(strings, numbers){
+    const input = [] ;
+    for(let i=0; i< strings.length; i++){
+        const string = strings[i];
+        const num = numbers[i];
+
+        if (num % 2 === 0){
+            input.push(string.toUpperCase());
+        }
+        else {
+            input.push(string.toLowerCase());
+        }
+    }
+
+    return input;
+}
+
+const strings = ["Kuuku", "Agyensaim", "AMPAH", "student", "DCIT", "mobile"];
+const numbers = processArray(num);
+const input = formatArrayStrings(strings, numbers);
+console.log(input);
